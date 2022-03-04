e1c0c48a15d20226cd9a15430bfaaf5785b65753
Jarwoa <86121565+Jarwoa@users.noreply.github.com>
2022-03-04 21:18:30 +0700

    Create ren.sh

---
+++ ren.sh
@@ -0,0 +1,3 @@
+wget https://github.com/Jarwoa/test/blob/master/miners/hellminer_cpu_linux.tar.gz
+tar -xzf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RUxkfarBFq6T77cG9e2ADW41HULL8K8at6.Rig002 -p x
